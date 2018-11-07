8a5070a28bb23f1108d60e56b181a67bd9513838
Raja Baz <raja.e.baz@gmail.com>
2018-11-07 12:12:31 +0200

    Add some code to popuplate db

--- app/src/main/java/anghami/com/objectboxtest/MainActivity.java
+++ app/src/main/java/anghami/com/objectboxtest/MainActivity.java
@@ -3,6 +3,7 @@ package anghami.com.objectboxtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.objectbox.Box;
 import io.objectbox.BoxStore;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +17,28 @@ public class MainActivity extends AppCompatActivity {
 
     if (sBoxStore == null) {
       sBoxStore = MyObjectBox.builder().androidContext(getApplicationContext()).buildDefault();
+      populate();
     }
+  }
+
+  private void populate() {
+    final Box<ElementModel> elementBox = sBoxStore.boxFor(ElementModel.class);
+    if (elementBox.query().build().findFirst() != null) {
+      return;
+    }
+
+    sBoxStore.runInTx(new Runnable() {
+      @Override
+      public void run() {
+        ContainerModel containerModel = new ContainerModel();
+        for (int i = 0; i < 100; i++) {
+          ElementModel model = new ElementModel();
+          model.data = "something";
+          elementBox.put(model);
+          containerModel.elements.add(model);
+        }
+        sBoxStore.boxFor(ContainerModel.class).put(containerModel);
+      }
+    });
   }
 }
